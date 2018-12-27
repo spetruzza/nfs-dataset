3ad1df4d0a4a55bf37d5078a7eaeeb16b0001ced
spetruzza <spetruzza@users.noreply.github.com>
2018-12-27 11:10:15 -0700

    Update profile.py

--- profile.py
+++ profile.py
@@ -60,7 +60,7 @@ nfsServer.addService(pg.Execute(shell="sh", command="sudo /bin/bash /local/repos
 
 # Special node that represents the ISCSI device where the dataset resides
 dsnode = request.RemoteBlockstore("dsnode", nfsDirectory)
-dsnode.dataset = "urn:publicid:IDN+emulab.net:portalprofiles+ltdataset+DemoDataset"
+dsnode.dataset = "urn:publicid:IDN+wisc.cloudlab.us:neon-data-portal-pg0+ltdataset+neon-wisc"
 
 # Link between the nfsServer and the ISCSI device that holds the dataset
 dslink = request.Link("dslink")
